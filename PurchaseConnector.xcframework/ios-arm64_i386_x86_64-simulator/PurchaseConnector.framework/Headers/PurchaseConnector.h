67ad4079a398afdd062d3ba5ac6e1c9f976f1cf8
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2022-08-15 14:48:26 +0300

    Purchase Connector v6.8.0

--- PurchaseConnector.xcframework/ios-arm64_i386_x86_64-simulator/PurchaseConnector.framework/Headers/PurchaseConnector.h
+++ PurchaseConnector.xcframework/ios-arm64_i386_x86_64-simulator/PurchaseConnector.framework/Headers/PurchaseConnector.h
@@ -1,12 +1,11 @@
 //
-//  AppsFlyerARS.h
-//  AppsFlyerARS-Dynamic
+//  PurchaseConnector.h
+//  PurchaseConnector project
 //
 //  Created by ivan.obodovskyi on 16.02.2022.
 //
 
 #import <Foundation/Foundation.h>
-
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -14,20 +13,16 @@ NS_ASSUME_NONNULL_BEGIN
 // Purchase revenue API
 typedef NS_OPTIONS(NSInteger, AFSDKAutoLogPurchaseRevenueOptions) {
     AFSDKAutoLogPurchaseRevenueOptionsDisabled = 0,
-    AFSDKAutoLogPurchaseRevenueOptionsRenewable = 1 << 0,
-    AFSDKAutoLogPurchaseRevenueOptionsCounsumable NS_UNAVAILABLE = 1 << 1,
-    AFSDKAutoLogPurchaseRevenueOptionsNonCounsumable NS_UNAVAILABLE = 1 << 2,
-    AFSDKAutoLogPurchaseRevenueOptionsNonRenewing NS_UNAVAILABLE = 1 << 3,
+    AFSDKAutoLogPurchaseRevenueOptionsAutoRenewableSubscriptions = 1 << 0,
+    AFSDKAutoLogPurchaseRevenueOptionsInAppPurchases = 1 << 1,
 } NS_SWIFT_NAME(AutoLogPurchaseRevenueOptions);
 
 
 NS_SWIFT_NAME(PurchaseRevenueDelegate)
 @protocol AppsFlyerPurchaseRevenueDelegate <NSObject>
 
-
 @optional
 - (void)didReceivePurchaseRevenueValidationInfo:(NSDictionary * _Nullable)validationInfo error:(NSError * _Nullable)error;
-
 
 @end
 
@@ -37,28 +32,27 @@ NS_SWIFT_NAME(PurchaseRevenueDelegate)
 NS_SWIFT_NAME(PurchaseRevenueDataSource)
 @protocol AppsFlyerPurchaseRevenueDataSource <NSObject>
 
-
 @optional
 - (NSDictionary * _Nullable)purchaseRevenueAdditionalParametersForProducts:(NSSet<SKProduct *> *_Nonnull)products
                                                               transactions:(NSSet<SKPaymentTransaction *> *_Nullable)transactions;
 
-
 @end
-
 
 
 @interface PurchaseConnector : NSObject
 
-
 @property(nonatomic) AFSDKAutoLogPurchaseRevenueOptions autoLogPurchaseRevenue;
-@property(nonatomic) id<AppsFlyerPurchaseRevenueDelegate> purchaseRevenueDelegate;
-@property(nonatomic) id<AppsFlyerPurchaseRevenueDataSource> purchaseRevenueDataSource;
+@property(weak, nonatomic, nullable) id<AppsFlyerPurchaseRevenueDelegate> purchaseRevenueDelegate;
+@property(weak, nonatomic, nullable) id<AppsFlyerPurchaseRevenueDataSource> purchaseRevenueDataSource;
 @property(nonatomic) BOOL isSandbox;
 
++ (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
-- (void)startObservingTransactions;
-
 + (PurchaseConnector *)shared;
+
+- (void)startObservingTransactions;
+- (void)stopObservingTransactions;
+
 
 
 @end
