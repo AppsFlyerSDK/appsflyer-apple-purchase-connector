da8e3a6b89e0a52cd37df3d0c49fdd49e6befc0a
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-04-14 20:08:20 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,9 @@ import PackageDescription
 let package = Package(
     name: "PurchaseConnector",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PurchaseConnector",
             targets: ["PurchaseConnector"]),
@@ -16,11 +15,9 @@ let package = Package(
             targets: ["PurchaseConnector-Dynamic"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.0")
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "PurchaseConnector",
             path: "PurchaseConnector.xcframework"
