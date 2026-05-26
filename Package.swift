878b2be523205b5e013135df9d5e2c10444364c5
Ivan Obodovskyi <ivan.obodovskyi@appsflyer.com>
2026-05-26 14:46:31 +0300

    Bump PurchaseConnector version to 6.18.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["PurchaseConnector"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static.git", exact: "6.18.0")
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static.git", exact: "6.18.1")
     ],
     targets: [
         .binaryTarget(
